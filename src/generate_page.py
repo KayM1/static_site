4ac8b194b3593a041db7470557e9dfbc002d2503
KayM1 <kay.meulders1@telenet.be>
2024-05-11 22:35:55 +0200

    static_site project done

--- src/generate_page.py
+++ src/generate_page.py
@@ -5,26 +5,33 @@ from pathlib import Path
 
 def generate_page(from_path, template_path, dest_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
-    from_file = open(from_path, "r")
-    markdown_content = from_file.read()
-    from_file.close()
+    
+    # Read Markdown content from file
+    with open(from_path, "r") as from_file:
+        markdown_content = from_file.read()
 
-    template_file = open(template_path, "r")
-    template = template_file.read()
-    template_file.close()
+    # Read template content from file
+    with open(template_path, "r") as template_file:
+        template = template_file.read()
 
+    # Generate HTML content from Markdown
     node = markdown_to_html_node(markdown_content)
     html = node.to_html()
 
+    # Extract title from Markdown content
     title = extract_title(markdown_content)
+
+    # Replace placeholders in the template with title and HTML content
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
 
+    # Create destination directory if it doesn't exist
     dest_dir_path = os.path.dirname(dest_path)
-    if dest_dir_path != "":
-        os.makedirs(dest_dir_path, exist_ok=True)
-    to_file = open(dest_path, "w")
-    to_file.write(template)
+    os.makedirs(dest_dir_path, exist_ok=True)
+
+    # Write HTML content to destination file
+    with open(dest_path, "w") as to_file:
+        to_file.write(template)
 
 
 def extract_title(md):
@@ -35,10 +42,11 @@ def extract_title(md):
     raise ValueError("No title found")
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    for entry in os.listdir(dir_path_content):
-        print(entry)
-        full_path = os.path.join(dir_path_content, entry)
-        if os.path.isfile(full_path) and entry.endswith('.md'):
-            # Generate page for each markdown file found
-            dest_file_path = os.path.join(dest_dir_path, f"{Path(entry).stem}.html")
-            generate_page(full_path, template_path, dest_file_path)
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
